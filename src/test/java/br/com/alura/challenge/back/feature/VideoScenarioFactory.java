7e6f0b3acbdb1e1501b63daa24aecead2a2483e0
Fábio Kopezinski <fkcarvalho>
2021-07-20 00:23:58 -0300

    test:Criação dos testes do post do video

--- src/test/java/br/com/alura/challenge/back/feature/VideoScenarioFactory.java
+++ src/test/java/br/com/alura/challenge/back/feature/VideoScenarioFactory.java
@@ -3,11 +3,19 @@ package br.com.alura.challenge.back.feature;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.alura.challenge.back.domain.Video;
+import br.com.alura.challenge.back.domain.dto.request.VideoRequest;
 import br.com.alura.challenge.back.domain.dto.response.VideoResponse;
 
 public class VideoScenarioFactory {
 
     public static final List<VideoResponse> FIND_ALL = loadFindAll();
+
+    public static final VideoRequest CREATE_REQUEST = loadCreateRequest();
+
+    public static final Video VIDEO = loadVideo();
+
+    public static final VideoResponse VIDEO_RESPONSE = loadVideoResponse();
 
     private static List<VideoResponse> loadFindAll() {
 
@@ -18,5 +26,22 @@ public class VideoScenarioFactory {
         list.add(video);
 
         return list;
+    }
+
+    private static VideoResponse loadVideoResponse() {
+
+        VideoResponse video = new VideoResponse(1L, "title", "description", "url");
+
+        return video;
+    }
+
+    private static Video loadVideo() {
+
+        Video video = new Video(1L, "title", "description", "url");
+        return video;
+    }
+
+    private static VideoRequest loadCreateRequest() {
+        return VideoRequest.builder().title("title").description("description").url("url").build();
     }
 }
