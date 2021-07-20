ba9e34217cec7c65d45983121343a2f1fb486d74
Fábio Kopezinski <fkcarvalho>
2021-07-20 15:03:06 -0300

    fix: ajuste do campo url

--- src/main/java/br/com/alura/challenge/back/domain/dto/request/VideoUpdate.java
+++ src/main/java/br/com/alura/challenge/back/domain/dto/request/VideoUpdate.java
@@ -1,5 +1,9 @@
 package br.com.alura.challenge.back.domain.dto.request;
 
+import org.hibernate.validator.constraints.URL;
+
+import br.com.alura.challenge.back.validations.OnCreate;
+import br.com.alura.challenge.back.validations.OnUpdate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +15,6 @@ public class VideoUpdate {
 
     private String description;
 
+    @URL(groups = { OnCreate.class, OnUpdate.class }, message = "Colocar em formato url")
     private String url;
 }
