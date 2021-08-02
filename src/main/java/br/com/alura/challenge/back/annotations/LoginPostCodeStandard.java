5f7d0e65f5e67eacdb3ec17cd5d771d3a9df893f
Fábio Kopezinski <fkcarvalho>
2021-08-02 16:47:12 -0300

    feat: listar free

--- src/main/java/br/com/alura/challenge/back/annotations/LoginPostCodeStandard.java
+++ src/main/java/br/com/alura/challenge/back/annotations/LoginPostCodeStandard.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/br/com/alura/challenge/back/service/VideoService.java
+++ src/main/java/br/com/alura/challenge/back/service/VideoService.java
@@ -1,7 +1,5 @@
 package br.com.alura.challenge.back.service;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
